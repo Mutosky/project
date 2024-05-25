3ce4bbaac258147dcfb0b025fef41b7b5a6258b7
tboysmokes <omotomiwar@gmail.com>
2024-05-25 16:25:21 +0100

    FPS_API configuration

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from init import flaskinit, teams, add_user, login_users, get_allusers, get_user, updateUser, authAdmin
 from flask import request, jsonify, render_template, redirect, url_for
 from flask_login import login_required, login_user, logout_user, UserMixin, LoginManager
+import requests
 from datetime import date
 
 app = flaskinit()
@@ -43,11 +44,73 @@ def geetin(data):
 @app.route('/footballanalysis', methods=['GET', 'POST'])
 @login_required
 def footballanalysis():
+    urlPastFiveMatch = 'http://fpsapi.pythonanywhere.com/pastfivematch'
+    urlHead2Head = 'http://fpsapi.pythonanywhere.com/Head2Head'
+    urlSimilarOpponent = 'http://fpsapi.pythonanywhere.com/similaropponent'
+    urlHomeAdvantages = 'http://fpsapi.pythonanywhere.com/homeadvantage'
     if request.method == 'POST':
         data = request.get_json()
         if "team1" and "team2" in data:                
             team1id = int(teams[data['team1']])
             team2id = int(teams[data['team2']])
+            json = {'team1': team1id, 'team2': team2id}
+
+            H2Hdata = requests.post(url=urlHead2Head, json=json)
+            PFMdata = requests.post(url=urlPastFiveMatch, json=json)
+            PFM2data = requests.post(url=urlPastFiveMatch, json=json)
+            SMOdata = requests.post(url=urlSimilarOpponent, json=json)
+            HAGdata = requests.post(url=urlHomeAdvantages, json=json)
+            print(H2Hdata.json())
+            print(PFM2data.json())
+            print(PFMdata.json())
+            print(SMOdata.json())
+            print(HAGdata.json())
+
+            probability = H2Hdata.json()['datalist']
+            outcome = H2Hdata.json()['match_outcome']
+            dates = H2Hdata.json()['event_date']
+            team1 = H2Hdata.json()['team1_id']
+            team2 = H2Hdata.json()['team2_id']
+
+            win1 = PFMdata.json()['win']
+            loss1 = PFMdata.json()['loss']
+            draw1 = PFMdata.json()['draw']
+            team1OpponentName = PFMdata.json()['opponent']
+            team1MatchOutcome = PFMdata.json()['outcome']
+            team1MatchDate = PFMdata.json()['date']
+            win2 = PFM2data.json()['win']
+            loss2 = PFM2data.json()['loss']
+            draw2 = PFM2data.json()['draw']
+            team2OpponentName = PFM2data.json()['opponent']
+            team2MatchOutcome = PFM2data.json()['outcome']
+            team2MatchDate = PFM2data.json()['date']
+
+            win3 = SMOdata.json()['team1']['win']
+            loss3 = SMOdata.json()['team1']['loss']
+            draw3 = SMOdata.json()['team1']['draw']
+            win4 = SMOdata.json()['team2']['win']
+            loss4 = SMOdata.json()['team2']['loss']
+            draw4 = SMOdata.json()['team2']['draw']
+            similarTeams = SMOdata.json()['opponent']['name']
+            datalist = SMOdata.json()['opponent']['date']
+
+            homeWin = HAGdata.json()['win']
+            homeLoss = HAGdata.json()['loss']
+            homeDraw = HAGdata.json()['draw']
+            eventDate = HAGdata.json()['date']
+            homeOutcome = HAGdata.json()['outcome']
+            opponent = HAGdata.json()['date']
+
+
+            return jsonify({'H2H_data': {'last_match_probability': probability, 'outcome': outcome, 'date': dates, 'team1': team1, 'team2': team2},
+                            'Lastfivematchdata': {'HomeTeam': {'win': win1, 'loss': loss1, 'draw': draw1, 'opponent': team1OpponentName, 'Outcome': team1MatchOutcome, 'date': team1MatchDate},
+                                                  'AwayTeam': {'win': win2, 'loss': loss2, 'draw': draw2, 'opponent': team2OpponentName, 'Outcome': team2MatchOutcome, 'date': team2MatchDate}},
+                            'homeAdvange': {'win': homeWin, 'loss': homeLoss, 'draw': homeDraw, 'date': eventDate, 'match_outcome': homeOutcome, 'opponent': opponent},
+                            'similarOpponent': {'teamOne': {'win': win3, 'loss': loss3, 'draw': draw3},
+                                                'teamTwo': {'win': win4, 'loss': loss4, 'draw': draw4},
+                                                'opponents': {'name': similarTeams, 'data': datalist}}
+                            })
+        else:
             return jsonify({'error': 'not data found'})
                 
     return render_template('footballanalysis.html')
